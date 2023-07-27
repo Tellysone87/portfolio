e7174444c1865879559ebe2077341f64fe93df8e
Tellysone87 <walterdtrt@aol.com>
2023-07-27 19:01:11 -0400

    simple flask server with base skeleton running

--- server.py
+++ server.py
@@ -1,1 +1,20 @@
 # flask server files
+from flask import Flask
+from flask import request, render_template # importing needed flask functions
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+
+
+@app.route('/')
+def load_page():
+    """Return home page"""
+
+    return render_template("base.html")
+
+
+if __name__ == '__main__': # running flask web application
+    app.run(debug=True, host='0.0.0.0')
