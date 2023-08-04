64cebd701e8dc46e20261f8bb5f6264d0626a47b
Tellysone87 <walterdtrt@aol.com>
2023-08-04 15:44:21 -0400

    updated website pages and added routes to server.

--- server.py
+++ server.py
@@ -7,14 +7,29 @@ app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
-
-
 @app.route('/')
 def load_page():
     """Return home page"""
 
     return render_template("base.html")
 
+@app.route('/project')
+def load_project_page():
+    """Return project page"""
+
+    return render_template("project.html")
+
+@app.route('/education')
+def load_education_page():
+    """Return education page"""
+
+    return render_template("education.html")
+
+@app.route('/contact')
+def load_contact_page():
+    """Return contact page"""
+
+    return render_template("contact.html")
 
 if __name__ == '__main__': # running flask web application
     app.run(debug=True, host='0.0.0.0')
