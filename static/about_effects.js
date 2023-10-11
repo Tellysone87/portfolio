59abe9e3eda61d6bffad0469c139ce9d660a70fb
Tellysone87 <walterdtrt@aol.com>
2023-10-10 21:50:20 -0400

    website updated with some notes

--- static/about_effects.js
+++ static/about_effects.js
@@ -1,47 +1,48 @@
-// function to make logo bigger when user hovers over them
-let pythonLogo = document.querySelectorAll(".logoGroup");
+// Date created: 10/4/2023
+// last updated 10/10/2023
+// Author: Shantel williams
+// File Name:about_effects.js
+// Connected to base.html
 
+let pythonLogo = document.querySelectorAll(".logoGroup"); // grabs all elements belonging to the logo group class
 
 // functions to make logo borders larger if mouse hovers over them
-pythonLogo.forEach(function(img) {
-    
+pythonLogo.forEach(function (img) {
 
-        // setting dictionary/object to store each example of a tech by logo { logo html element ID : list of projects utilizing it}
+    // setting dictionary/object to store each example of a tech by logo 
     langaugeExmple = {
-        pythonLogo: ["pack Track","Under Pressure"],
-        CLogo: ["Just Escape","Telly's Driver Ed"],
+        // { logo html element ID : list of projects utilizing it}
+        pythonLogo: ["pack Track", "Under Pressure"],
+        CLogo: ["Just Escape", "Telly's Driver Ed"],
         jsLogo: ["pack Track"],
         cssLogo: ["pack Track"],
         htmlLogo: ["pack Track"]
-}
+    }
 
-// adding event listener to each logo img for mouse enter event
-    img.addEventListener("mouseenter", function (){
+    // adding event listener to each logo img for mouse enter event
+    img.addEventListener("mouseenter", function () {
         img.style.border = "Thick solid black"; // Changing border, border width and getting that particular image id
         img.style.borderWidth = "6px";
         let define_logo = img.getAttribute('id');
 
         const pList = langaugeExmple[define_logo]; // setting an variable to the value of the key matching the id
         let projectHolder = document.createElement("ul"); // creating list to hold the item of the value list 
-        projectHolder.id ="hold_project" // setting list id 
+        projectHolder.id = "hold_project" // setting list id 
         img.parentNode.append(projectHolder) // appending that list to the image parent node or column
-        
+
         let i = 0
-        while(i < pList.length){ // grabbing each project in the list 
+        while (i < pList.length) { // grabbing each project in the list 
             let example = document.createElement("li"); // create a list element
-            example.innerHTML = pList[i] // displat the project in the list element
-            projectHolder.append(example) // appending the list object ot the unordered list 
-            i++
-
+            example.innerHTML = pList[i]; // displat the project in the list element
+            projectHolder.append(example); // appending the list object ot the unordered list 
+            i++;
         }
- 
-    });
-    
-    //hides the bullet points of projects once user removes mouse off the video
-    img.addEventListener("mouseout", function (){
-        img.style.borderWidth ="2px"; // border returns to normal 
-        projectHolder.style.visibility = "hidden" // list is hidden
-      
     });
 
+    //hides the bullet points of projects once user removes mouse off the video
+    img.addEventListener("mouseout", function () {
+        img.style.borderWidth = "2px"; // border returns to normal 
+        const projectHolders = document.getElementById("hold_project"); // locate unordered list
+        projectHolders.remove(); // remove the created list of projects
+    });
 });
--- static/project_effects.js
+++ static/project_effects.js
@@ -1,17 +1,19 @@
 // Date created: 10/4/2023
-// Name:Shantel Williams
-// Description: Javascript file to help have some effects and responsive elements
+// last updated 10/10/2023
+// Author: Shantel williams
+// File Name:project_effects.js
+// Connected to project.html
 
 let packSkills = document.getElementById("packVOD");// grabs pack yt video player
 
 // display the bullet points of pack track once user hovers the video
-packSkills.addEventListener("mouseenter", function (){
+packSkills.addEventListener("mouseenter", function () {
 
     let highlight = ["Designed and engineered a web application that allows you to track all your packages and view them on a google map.",
-     "Utilized html, PostgreSQL, JavaScript, CSS, Bootstrap, Ajax, and jinja to successfully develop my fullstack application.", 
-     "Learned time management, web development, API integration and backend programming."];
+        "Utilized html, PostgreSQL, JavaScript, CSS, Bootstrap, Ajax, and jinja to successfully develop my fullstack application.",
+        "Learned time management, web development, API integration and backend programming."];
 
-    let unorderList = document.getElementById("packListItems"); 
+    let unorderList = document.getElementById("packListItems");
 
     i = 0 // Set index to zero
 
@@ -19,7 +21,7 @@ packSkills.addEventListener("mouseenter", function (){
 })
 
 // display the bullet points of pack track once user removes mouse off the video
-packSkills.addEventListener("mouseout", function (){
+packSkills.addEventListener("mouseout", function () {
 
     let listI = document.getElementsByClassName("packItem");
     let ulist = document.getElementById("packListItems");
@@ -28,17 +30,17 @@ packSkills.addEventListener("mouseout", function (){
 })
 
 // funvtion to take an element and list and create list items in the element
-function DisplayItems(ele,listnum){
-   
-    while (i < listnum.length){
+function DisplayItems(ele, listnum) {
+
+    while (i < listnum.length) {
         newItem = document.createElement("li");
         newItem.innerHTML = listnum[i];
         ele.appendChild(newItem);
-        newItem.className ="packItem";
+        newItem.className = "packItem";
         newItem.visibility = "hidden"
         i++;
-    }  
-    
+    }
+
 }
 
 // Fucntion to remove an elements children
