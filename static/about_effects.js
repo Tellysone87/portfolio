d4ecc49ef421c9ae15e608931a8708f8d707ee9f
Tellysone87 <walterdtrt@aol.com>
2023-10-24 16:00:33 -0400

    portfolio updates

--- static/about_effects.js
+++ static/about_effects.js
@@ -12,11 +12,11 @@ pythonLogo.forEach(function (img) {
     // setting dictionary/object to store each example of a tech by logo 
     langaugeExmple = {
         // { logo html element ID : list of projects utilizing it}
-        pythonLogo: ["pack Track", "Under Pressure"],
+        pythonLogo: ["Pack Track", "Under Pressure"],
         CLogo: ["Just Escape", "Telly's Driver Ed"],
-        jsLogo: ["pack Track"],
-        cssLogo: ["pack Track"],
-        htmlLogo: ["pack Track"]
+        jsLogo: ["Pack Track"],
+        cssLogo: ["Pack Track"],
+        htmlLogo: ["Pack Track"]
     }
 
     // adding event listener to each logo img for mouse enter event
@@ -26,15 +26,48 @@ pythonLogo.forEach(function (img) {
         let define_logo = img.getAttribute('id');
 
         const pList = langaugeExmple[define_logo]; // setting an variable to the value of the key matching the id
-        let projectHolder = document.createElement("ul"); // creating list to hold the item of the value list 
-        projectHolder.id = "hold_project" // setting list id 
-        img.parentNode.append(projectHolder) // appending that list to the image parent node or column
+        const summary = document.getElementById("photo_col") // getting photo column
 
+        //////////////////// Create row div //////////////////////////////////////////
+        let newRow = document.createElement("div"); // create row for summary
+        newRow.className ="row"; // set row class name
+        newRow.id ="summary_row"; // set thw row id
+        summary.append(newRow); // append row to my photo column
+
+         //////////////////////////////////////create title for table ////////////////////////
+         const table_title = document.createElement("h6");
+         table_title.innerText = `Projects utilizing ${define_logo}`;
+         newRow.append(table_title);
+
+        /////////////////////////// Create table /////////////////////////////////////////////
+        const table = document.createElement("table"); // create a table for my project examples
+        table.id ="project_table";// table id
+        const assigned_row = document.getElementById("summary_row")
+        assigned_row.append(table); //append our table to the row
+
+        const project_table = document.getElementById("project_table"); //  grabs table
+        
+        //////////////////////// create table row ////////////////////////////////////
+        const table_row = document.createElement("tr"); // create a table row
+        table_row.id ="the_row";
+
+        //////////////////////// Create header ///////////////////////////////////////
+        const header = document.createElement("th"); // create a table header
+        header.innerText ="Name";
+        header.id ="header"
+
+        ////////////////////////////// add the elements //////////////////////////////
+        project_table.append(table_row);
+        table_row.append(header);
+
+        ////////////////// loop thorugh info//////////////////////////////////////////////
         let i = 0
         while (i < pList.length) { // grabbing each project in the list 
-            let example = document.createElement("li"); // create a list element
-            example.innerHTML = pList[i]; // displat the project in the list element
-            projectHolder.append(example); // appending the list object ot the unordered list 
+            let another_row = document.createElement("tr");
+            let example = document.createElement("td"); // create a list element
+            example.innerHTML = pList[i]; // display the project in the list element
+            another_row.append(example) ; //Append to the row
+            project_table.append(another_row);
             i++;
         }
     });
@@ -42,7 +75,7 @@ pythonLogo.forEach(function (img) {
     //hides the bullet points of projects once user removes mouse off the video
     img.addEventListener("mouseout", function () {
         img.style.borderWidth = "2px"; // border returns to normal 
-        const projectHolders = document.getElementById("hold_project"); // locate unordered list
-        projectHolders.remove(); // remove the created list of projects
+        const remove_summary = document.getElementById("summary_row") // grabbing list under picture
+        remove_summary.remove(); // remove list
     });
 });
